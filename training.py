e5973eeacabe99188beccd533a9f80cf2da01e2a
Quan Vu <nongdenchet8435848@gmail.com>
2018-03-12 22:06:14 +0700

    test more models

--- training.py
+++ training.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from collections import Counter
@@ -45,36 +44,47 @@ def guess(url, dataset, model):
     return result[0] == 1, model.predict_proba(matrix)
 
 
+# Training
 def main():
+    # Prepare data
     dataset = pd.read_csv('input.csv')
     X = features(dataset)
     y = labels(dataset)
-
-    # Splitting data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    # Training
+    # Naive Bayes
+    from sklearn.naive_bayes import MultinomialNB
     model1 = MultinomialNB()
     model1.fit(X_train, y_train)
     result1 = model1.predict(X_test)
-
-    model2 = GaussianNB()
+    
+    # KNN
+    from sklearn.neighbors import KNeighborsClassifier
+    model2 = KNeighborsClassifier(n_neighbors=10, metric='minkowski', p=2)
     model2.fit(X_train, y_train)
     result2 = model2.predict(X_test)
 
-    model3 = BernoulliNB()
+    # SVM
+    from sklearn.svm import SVC
+    model3 = SVC(kernel='rbf', random_state=0)
     model3.fit(X_train, y_train)
     result3 = model3.predict(X_test)
+    
+    # Random Forest
+    from sklearn.ensemble import RandomForestClassifier
+    model4 = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0)
+    model4.fit(X_train, y_train)
+    result4 = model4.predict(X_test)
 
     # Assert
-    print("Confusion_matrix: MultinomialNB() ", confusion_matrix(y_test, result1))
-    print("Confusion_matrix: GaussianNB()", confusion_matrix(y_test, result2))
-    print("Confusion_matrix: BernoulliNB()", confusion_matrix(y_test, result3))
-
-    print("acc of using model: MultinomialNB() is : ", accuracy_score(y_test, result1))
-    print("acc of using model: GaussianNB() is : ", accuracy_score(y_test, result2))
-    print("Confusion_matrix: BernoulliNB()", accuracy_score(y_test, result3))
-
-
-# Process
-main()
+    print("Confusion_matrix: MultinomialNB ", confusion_matrix(y_test, result1))
+    print("Confusion_matrix: KNeighborsClassifier ", confusion_matrix(y_test, result2))
+    print("Confusion_matrix: SVC ", confusion_matrix(y_test, result3))
+    print("Confusion_matrix: RandomForestClassifier ", confusion_matrix(y_test, result3))
+    
+    print("acc of using model: MultinomialNB is: ", accuracy_score(y_test, result1))
+    print("acc of using model: KNeighborsClassifier is: ", accuracy_score(y_test, result2))
+    print("Confusion_matrix: SVC is: ", accuracy_score(y_test, result3))
+    print("Confusion_matrix: RandomForestClassifier is: ", accuracy_score(y_test, result4))
+    
+    
